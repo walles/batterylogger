fd6ac244a20ce7f72d1e443871d9820a74626221
Johan Walles <johan.walles@gmail.com>
2014-07-05 15:16:03 +0200

    Start implementing the History class
    This implementation passes two out of six test cases.

--- src/androidTest/java/com/gmail/walles/johan/batterylogger/HistoryTest.java
+++ src/androidTest/java/com/gmail/walles/johan/batterylogger/HistoryTest.java
@@ -7,14 +7,6 @@ import java.util.Arrays;
 import java.util.Date;
 
 public class HistoryTest extends TestCase {
-    private final static long HOUR_MS = 1000 * 3600;
-
-    public void testBlank() {
-        History testMe = new History();
-        assertEquals(0, testMe.getBatteryDrain().size());
-        assertEquals(0, testMe.getEvents().size());
-    }
-
     private void assertValues(XYSeries batteryDrain, double ... expectedValues) {
         // FIXME: This method should go via persistent storage to verify that as well
         double actualValues[] = new double[batteryDrain.size()];
@@ -42,28 +34,34 @@ public class HistoryTest extends TestCase {
         assertEquals(Arrays.toString(expectedDescriptions), Arrays.toString(actualDescriptions));
     }
 
+    public void testBlank() {
+        History testMe = new History();
+        assertEquals(0, testMe.getBatteryDrain().size());
+        assertEquals(0, testMe.getEvents().size());
+    }
+
     public void testOnlyBatteryEvents() {
         History testMe = new History();
-        testMe.addBatteryLevelEvent(100, new Date(1 * HOUR_MS));
+        testMe.addBatteryLevelEvent(100, new Date(1 * History.HOUR_MS));
         assertEquals(0, testMe.getBatteryDrain().size());
         assertEquals(0, testMe.getEvents().size());
 
-        testMe.addBatteryLevelEvent(98, new Date(3 * HOUR_MS));
+        testMe.addBatteryLevelEvent(98, new Date(3 * History.HOUR_MS));
         assertEquals(1, testMe.getBatteryDrain().size());
         assertEquals(0, testMe.getEvents().size());
 
         XYSeries batteryDrainSeries = testMe.getBatteryDrain().get(0);
         // Drain timestamp should be between the sample timestamps
-        assertTimestamps(batteryDrainSeries, new Date(2 * HOUR_MS));
+        assertTimestamps(batteryDrainSeries, new Date(2 * History.HOUR_MS));
         // From 100% to 98% in two hours = 1%/h
         assertValues(batteryDrainSeries, 1.0);
 
-        testMe.addBatteryLevelEvent(94, new Date(5 * HOUR_MS));
+        testMe.addBatteryLevelEvent(94, new Date(5 * History.HOUR_MS));
         assertEquals(1, testMe.getBatteryDrain().size());
         assertEquals(0, testMe.getEvents().size());
 
         batteryDrainSeries = testMe.getBatteryDrain().get(0);
-        assertTimestamps(batteryDrainSeries, new Date(2 * HOUR_MS), new Date(4 * HOUR_MS));
+        assertTimestamps(batteryDrainSeries, new Date(2 * History.HOUR_MS), new Date(4 * History.HOUR_MS));
         // From 100% to 98% in two hours = 1%/h
         // From 98% to 94% in two hours = 2%/h
         assertValues(batteryDrainSeries, 1.0, 2.0);
@@ -71,85 +69,85 @@ public class HistoryTest extends TestCase {
 
     public void testChargingEvents() {
         History testMe = new History();
-        testMe.addBatteryLevelEvent(51, new Date(1 * HOUR_MS));
-        testMe.addBatteryLevelEvent(50, new Date(3 * HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(1 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(3 * History.HOUR_MS));
 
-        testMe.addStartChargingEvent(new Date(5 * HOUR_MS));
-        testMe.addBatteryLevelEvent(51, new Date(7 * HOUR_MS));
-        testMe.addStopChargingEvent(new Date(9 * HOUR_MS));
+        testMe.addStartChargingEvent(new Date(5 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(7 * History.HOUR_MS));
+        testMe.addStopChargingEvent(new Date(9 * History.HOUR_MS));
 
-        testMe.addBatteryLevelEvent(50, new Date(11 * HOUR_MS));
-        testMe.addBatteryLevelEvent(47, new Date(13 * HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(11 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(47, new Date(13 * History.HOUR_MS));
 
         assertEquals(2, testMe.getBatteryDrain().size());
         assertValues(testMe.getBatteryDrain().get(0), 0.5);
-        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(2 * HOUR_MS));
+        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(2 * History.HOUR_MS));
         assertValues(testMe.getBatteryDrain().get(1), 1.5);
-        assertTimestamps(testMe.getBatteryDrain().get(1), new Date(12 * HOUR_MS));
+        assertTimestamps(testMe.getBatteryDrain().get(1), new Date(12 * History.HOUR_MS));
 
-        assertTimestamps(testMe.getEvents(), new Date(5 * HOUR_MS), new Date(9 * HOUR_MS));
+        assertTimestamps(testMe.getEvents(), new Date(5 * History.HOUR_MS), new Date(9 * History.HOUR_MS));
         assertDescriptions(testMe.getEvents(), "Start charging", "Stop charging");
     }
 
     public void testMissingStartChargingEvent() {
         History testMe = new History();
-        testMe.addBatteryLevelEvent(51, new Date(1 * HOUR_MS));
-        testMe.addBatteryLevelEvent(50, new Date(3 * HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(1 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(3 * History.HOUR_MS));
 
-        testMe.addBatteryLevelEvent(51, new Date(7 * HOUR_MS));
-        testMe.addStopChargingEvent(new Date(9 * HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(7 * History.HOUR_MS));
+        testMe.addStopChargingEvent(new Date(9 * History.HOUR_MS));
 
-        testMe.addBatteryLevelEvent(50, new Date(11 * HOUR_MS));
-        testMe.addBatteryLevelEvent(47, new Date(13 * HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(11 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(47, new Date(13 * History.HOUR_MS));
 
         // Everything before the stop charging event should be ignored
         assertEquals(1, testMe.getBatteryDrain().size());
         assertValues(testMe.getBatteryDrain().get(0), 1.5);
-        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(12 * HOUR_MS));
+        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(12 * History.HOUR_MS));
 
-        assertTimestamps(testMe.getEvents(), new Date(9 * HOUR_MS));
+        assertTimestamps(testMe.getEvents(), new Date(9 * History.HOUR_MS));
         assertDescriptions(testMe.getEvents(), "Stop charging");
     }
 
     public void testRebootEvents() {
         History testMe = new History();
-        testMe.addBatteryLevelEvent(51, new Date(1 * HOUR_MS));
-        testMe.addBatteryLevelEvent(50, new Date(3 * HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(1 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(3 * History.HOUR_MS));
 
-        testMe.addSystemHaltingEvent(new Date(5 * HOUR_MS));
-        testMe.addBatteryLevelEvent(51, new Date(7 * HOUR_MS));
-        testMe.addSystemBootingEvent(new Date(9 * HOUR_MS));
+        testMe.addSystemHaltingEvent(new Date(5 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(7 * History.HOUR_MS));
+        testMe.addSystemBootingEvent(new Date(9 * History.HOUR_MS));
 
-        testMe.addBatteryLevelEvent(50, new Date(11 * HOUR_MS));
-        testMe.addBatteryLevelEvent(47, new Date(13 * HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(11 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(47, new Date(13 * History.HOUR_MS));
 
         assertEquals(2, testMe.getBatteryDrain().size());
         assertValues(testMe.getBatteryDrain().get(0), 0.5);
-        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(2 * HOUR_MS));
+        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(2 * History.HOUR_MS));
         assertValues(testMe.getBatteryDrain().get(1), 1.5);
-        assertTimestamps(testMe.getBatteryDrain().get(1), new Date(12 * HOUR_MS));
+        assertTimestamps(testMe.getBatteryDrain().get(1), new Date(12 * History.HOUR_MS));
 
-        assertTimestamps(testMe.getEvents(), new Date(5 * HOUR_MS), new Date(9 * HOUR_MS));
+        assertTimestamps(testMe.getEvents(), new Date(5 * History.HOUR_MS), new Date(9 * History.HOUR_MS));
         assertDescriptions(testMe.getEvents(), "System shutting down", "System starting up");
     }
 
     public void testMissingShutdownEvent() {
         History testMe = new History();
-        testMe.addBatteryLevelEvent(51, new Date(1 * HOUR_MS));
-        testMe.addBatteryLevelEvent(50, new Date(3 * HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(1 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(3 * History.HOUR_MS));
 
-        testMe.addBatteryLevelEvent(51, new Date(7 * HOUR_MS));
-        testMe.addSystemBootingEvent(new Date(9 * HOUR_MS));
+        testMe.addBatteryLevelEvent(51, new Date(7 * History.HOUR_MS));
+        testMe.addSystemBootingEvent(new Date(9 * History.HOUR_MS));
 
-        testMe.addBatteryLevelEvent(50, new Date(11 * HOUR_MS));
-        testMe.addBatteryLevelEvent(47, new Date(13 * HOUR_MS));
+        testMe.addBatteryLevelEvent(50, new Date(11 * History.HOUR_MS));
+        testMe.addBatteryLevelEvent(47, new Date(13 * History.HOUR_MS));
 
         // Everything before the system booting event should be ignored
         assertEquals(1, testMe.getBatteryDrain().size());
         assertValues(testMe.getBatteryDrain().get(0), 1.5);
-        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(12 * HOUR_MS));
+        assertTimestamps(testMe.getBatteryDrain().get(0), new Date(12 * History.HOUR_MS));
 
-        assertTimestamps(testMe.getEvents(), new Date(9 * HOUR_MS));
+        assertTimestamps(testMe.getEvents(), new Date(9 * History.HOUR_MS));
         assertDescriptions(testMe.getEvents(), "System starting up");
     }
 }
--- src/main/java/com/gmail/walles/johan/batterylogger/History.java
+++ src/main/java/com/gmail/walles/johan/batterylogger/History.java
@@ -1,52 +1,156 @@
 package com.gmail.walles.johan.batterylogger;
 
+import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYSeries;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 public class History {
-    public void addBatteryLevelEvent(int percentage, Date timestamp) {
+    public final static long HOUR_MS = 1000 * 3600;
 
+    private static class Event {
+        enum Type {
+            BATTERY_LEVEL,
+            CHARGING_START,
+            CHARGING_STOP,
+            SYSTEM_SHUTDOWN,
+            SYSTEM_BOOT
+        }
+
+        private final Date timestamp;
+        private final Type type;
+        private int percentage;
+
+        private Event(Date timestamp, Type type) {
+            this.timestamp = timestamp;
+            this.type = type;
+        }
+
+        public static Event createBatteryLevelEvent(int percentage, Date timestamp) {
+            Event event = new Event(timestamp, Type.BATTERY_LEVEL);
+            event.percentage = percentage;
+            return event;
+        }
+
+        public static Event createStartChargingEvent(Date timestamp) {
+            return new Event(timestamp, Type.CHARGING_START);
+        }
+
+        public static Event createStopChargingEvent(Date timestamp) {
+            return new Event(timestamp, Type.CHARGING_STOP);
+        }
+
+        public static Event createSystemHaltingEvent(Date timestamp) {
+            return new Event(timestamp, Type.SYSTEM_SHUTDOWN);
+        }
+
+        public static Event createSystemBootingEvent(Date timestamp) {
+            return new Event(timestamp, Type.SYSTEM_BOOT);
+        }
+    }
+
+    private final ArrayList<Event> events = new ArrayList<Event>();
+
+    public void addBatteryLevelEvent(int percentage, Date timestamp) {
+        events.add(Event.createBatteryLevelEvent(percentage, timestamp));
     }
 
     public void addStartChargingEvent(Date timestamp) {
-
+        events.add(Event.createStartChargingEvent(timestamp));
     }
 
     public void addStopChargingEvent(Date timestamp) {
-
+        events.add(Event.createStopChargingEvent(timestamp));
     }
 
     /**
      * System halting.
      */
     public void addSystemHaltingEvent(Date timestamp) {
-
+        events.add(Event.createSystemHaltingEvent(timestamp));
     }
 
     /**
      * System starting up.
      */
     public void addSystemBootingEvent(Date timestamp) {
-
+        events.add(Event.createSystemBootingEvent(timestamp));
     }
 
     /**
      * Add these series to a plot and you'll see how battery drain speed has changed over time.
      */
     public List<XYSeries> getBatteryDrain() {
-        return null;
+        SimpleXYSeries xySeries = new SimpleXYSeries("Battery Drain");
+
+        Event lastLevelEvent = null;
+        for (Event event : events) {
+            if (event.type != Event.Type.BATTERY_LEVEL) {
+                continue;
+            }
+
+            if (lastLevelEvent == null) {
+                lastLevelEvent = event;
+                continue;
+            }
+
+            double deltaHours =
+                    (event.timestamp.getTime() - lastLevelEvent.timestamp.getTime()) / (double)HOUR_MS;
+            double drain = (lastLevelEvent.percentage - event.percentage) / deltaHours;
+            Date drainTimestamp = new Date((event.timestamp.getTime() + lastLevelEvent.timestamp.getTime()) / 2);
+            xySeries.addLast(toDouble(drainTimestamp), drain);
+
+            lastLevelEvent = event;
+        }
+
+        if (xySeries.size() == 0) {
+            return new LinkedList<XYSeries>();
+        }
+
+        return new LinkedList<XYSeries>(Arrays.asList(xySeries));
     }
 
     /**
      * Add this to a plot and you'll hopefully see what events affect your battery usage.
      */
     public EventSeries getEvents() {
-        return null;
+        EventSeries returnMe = new EventSeries();
+        for (Event event : events) {
+            if (event.type == Event.Type.BATTERY_LEVEL) {
+                continue;
+            }
+
+            String description;
+            switch (event.type) {
+                case CHARGING_START:
+                    description = "Start charging";
+                    break;
+                case CHARGING_STOP:
+                    description = "Stop charging";
+                    break;
+                case SYSTEM_BOOT:
+                    description = "System starting up";
+                    break;
+                case SYSTEM_SHUTDOWN:
+                    description = "System shutting down";
+                    break;
+                default:
+                    description = "Unknown event type " + event.type;
+            }
+            returnMe.add(toDouble(event.timestamp), description);
+        }
+        return returnMe;
     }
 
     public static Date toDate(Number x) {
         return new Date(x.intValue() * 1000L);
+    }
+
+    public static double toDouble(Date timestamp) {
+        return timestamp.getTime() / 1000L;
     }
 }

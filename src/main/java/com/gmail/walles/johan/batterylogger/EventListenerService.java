fa9c42b33947f669f4fde0a0ed2fb776b82e4330
Johan Walles <johan.walles@gmail.com>
2014-07-07 19:40:42 +0200

    Log an event when battery logging starts

--- src/main/java/com/gmail/walles/johan/batterylogger/EventListenerService.java
+++ src/main/java/com/gmail/walles/johan/batterylogger/EventListenerService.java
@@ -6,6 +6,9 @@ import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.Date;
+
 import static com.gmail.walles.johan.batterylogger.MainActivity.TAG;
 
 /**
@@ -17,6 +20,13 @@ public class EventListenerService extends Service {
         super.onCreate();
 
         Log.i(TAG, "Event listener service starting...");
+
+        try {
+            new History(this).addInfoEvent("Battery logging started", new Date());
+        } catch (IOException e) {
+            // FIXME: Should we shut down the service when this happens?
+            Log.e(TAG, "Unable to log events", e);
+        }
 
         // Listen for changes to battery charge
         registerReceiver(new EventReceiver(), new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
